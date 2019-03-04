d95c6d61082c1902b3c1b52c96bd3cf197c1bdc2
Soulike <316413310@qq.com>
2019-03-04 21:33:35 +0800

    改变 url 前缀函数行为，增加前缀

--- src/Config/ROUTER/Function.js
+++ src/Config/ROUTER/Function.js
@@ -4,11 +4,29 @@ function prefixFunctionGenerator(prefix)
 {
     return url =>
     {
-        url = Function.removePrependSlashes(url);
-        return `/${prefix}/${url}`;
+        if (url)
+        {
+            url = Function.removePrependSlashes(url);
+            return `/${prefix}/${url}`;
+        }
+        else
+        {
+            return `/${prefix}`;
+        }
     };
 }
 
+// 帐号相关前缀
 export const accountPrefix = prefixFunctionGenerator('/account');
 
+// 个人（患者）相关前缀
 export const patientPrefix = prefixFunctionGenerator('/patient');
+
+// 保险公司相关前缀
+export const insuranceCompanyPrefix = prefixFunctionGenerator('/insuranceCompany');
+
+// 第三方服务商相关前缀
+export const serviceProviderPrefix = prefixFunctionGenerator('/serviceProvider');
+
+// 医院相关前缀
+export const hospitalProviderPrefix = prefixFunctionGenerator('/hospitalProvider');
