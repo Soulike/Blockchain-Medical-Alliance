cfce3e75610f34cf2d3b6b9f12b81d2c31c76dc2
Soulike <316413310@qq.com>
2019-03-05 20:20:37 +0800

    完成 Root 布局

--- src/Pages/Root/View.js
+++ src/Pages/Root/View.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 import Style from './Style.module.scss';
+import {View as Header} from './Header';
+import {View as Footer} from './Footer';
 
 class Root extends Component
 {
     render()
     {
+        const {children} = this.props;
         return (
             <div className={Style.Root}>
-
+                <header className={Style.headerWrapper}>
+                    <Header />
+                </header>
+                <main className={Style.mainContent}>
+                    {children}
+                </main>
+                <footer className={Style.footerWrapper}>
+                    <Footer />
+                </footer>
             </div>
         );
     }
