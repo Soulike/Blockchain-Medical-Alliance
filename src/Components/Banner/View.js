8fa3486482f0593421fa83399424473af37b206e
Soulike <316413310@qq.com>
2019-03-06 11:02:14 +0800

    Banner 按钮根据页面路径来决定是否显示

--- src/Components/Banner/View.js
+++ src/Components/Banner/View.js
@@ -1,19 +1,43 @@
 import React, {Component} from 'react';
 import Style from './Style.module.scss';
+import {ROUTER} from '../../Config';
+import {browserHistory} from 'react-router';
 
 class Banner extends Component
 {
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            shouldShowAddInsuranceButton: false,
+        };
+    }
+
+    componentDidMount()
+    {
+        this.setState({
+            shouldShowAddInsuranceButton: browserHistory.getCurrentLocation().pathname.toLowerCase() === ROUTER.PAGE_ID_TO_ROUTE[ROUTER.PAGE_ID.INSURANCE_COMPANY.INSURANCE_COMPANY_INSURANCE_LIST].toLowerCase(),
+        });
+    }
+
+
     render()
     {
+        const {shouldShowAddInsuranceButton} = this.state;
         return (
             <div className={Style.Banner}>
                 <div className={Style.sloganWrapper}>
                     <div className={Style.bigSlogan}>因为专业 所以信赖</div>
                     <div className={Style.smallSlogan}>您的满意是我们存在的价值</div>
                 </div>
-                <div className={Style.buttonWrapper}>
-                    <button className={Style.addInsuranceButton}>新建保险</button>
-                </div>
+                {
+                    shouldShowAddInsuranceButton
+                        ? (
+                            <div className={Style.buttonWrapper}>
+                                <button className={Style.addInsuranceButton}>新建保险</button>
+                            </div>)
+                        : null
+                }
             </div>
         );
     }
